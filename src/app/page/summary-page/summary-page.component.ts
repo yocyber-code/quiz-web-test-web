bba781e97570a02d059883f9494ef54c0d2370d7
yocyber-code <yocyberyoshi@gmail.com>
2024-05-20 20:57:12 +0700

    feat : summary

--- src/app/page/summary-page/summary-page.component.ts
+++ src/app/page/summary-page/summary-page.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { CoreService } from '../../core/core.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-summary-page',
   templateUrl: './summary-page.component.html',
-  styleUrl: './summary-page.component.scss'
+  styleUrl: './summary-page.component.scss',
 })
 export class SummaryPageComponent {
+  constructor(private readonly coreService: CoreService, private readonly router: Router) {}
 
+  username: string = '';
+  score: number = 0;
+  rank: number = 0;
+
+  async ngOnInit(): Promise<void> {
+    const identity = this.coreService.utilities.storage.getIdentity();
+    const resp = await this.coreService.api.quiz.getSummary(identity.id, identity.username, identity.group_id);
+    console.log(resp);
+    if (resp.results) {
+      this.username = resp.results.username;
+      this.score = resp.results.score;
+      this.rank = resp.results.rank;
+    } else {
+      this.router.navigate(['/home']);
+    }
+  }
 }
--- src/auth-guard.service.ts
+++ src/auth-guard.service.ts
@@ -9,6 +9,14 @@ export class AuthGuardService {
   async canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     const url: string = state.url;
     const identity = await this.coreService.utilities.storage.getIdentity();
+    if (!identity) {
+      if (url.includes('quiz')) {
+        this.router.navigate(['/home']);
+      }
+      if (url.includes('summary')) {
+        this.router.navigate(['/home']);
+      }
+    }
 
     return true;
   }
